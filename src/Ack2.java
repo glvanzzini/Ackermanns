b7cc2993408755278662cda9b262b4024b0a7df6
Giampiero <glvanzzini@gmail.com>
2016-10-08 23:56:07 -0500

    Documention and slight changes

--- src/Ack2.java
+++ src/Ack2.java
@@ -1,31 +1,35 @@
 /**
  * Created by Giampiero on 10/2/2016.
+ * Professor: Dr. Becnel
+ * Class: CSC 342
+ * Date Due: 10/11/16
+ * Program: Ackermann function(two parameter version)
+ * Purpose: To take in 2 command line integer inputs and return the value computed by the Ackermann function.
  */
 public class Ack2 {
     private static Timer timer = Timer.getInstance();
     public static void main(String[] args){
-        timer.startTimer();
+        timer.startTimer(); //starts nanosecond timer
+        int m = 0;
+        int n = 0;
+
         int[] arr;
-       if(args.length >= 2 && args.length <= 3){
-           arr = new int[args.length];
-           for(int i = 0; i < args.length; i++){
-                arr[i] = Integer.parseInt(args[i]);
-               System.out.print(arr[i] + " ");
-           }
-       }else {
-           arr = new int[2];
-           arr[0] = 3;
-           arr[1] = 2;
+       if(args.length >= 2){ //if command line arguements are passed in use them
+           m = Integer.parseInt(args[0]);
+           n = Integer.parseInt(args[1]);
+       }else { //if not just use 3 and 2
+           m = 3;
+           n = 2;
        }
 
-       System.out.println(Ack(arr[0], arr[1]));
+       System.out.println("Value computed: " + Ack(m, n)); //prints ackermann value
 
 
-        timer.stopTimer();
+        timer.stopTimer(); //stops and prints timer
     }
 
 
-    private static int Ack(int m, int n){
+    private static int Ack(int m, int n){ //this is the implementation of the ackermanns function
         if(m == 0){
              return n + 1;
         }
